4f0e57cc49b32da6b9a95323c0d4175fd1f4537b
Brian Reardon <Brian.D.Reardon@gmail.com>
2023-02-24 16:42:21 -0500

    done

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts){
+for (let i = 0; i < gifts.length; i++){
+    console.log(`wrapped ${gifts[i]} years old. Happy birthday to me!`);
+    //debugger
+}
+
+return gifts;
+}
+
+wrapGifts(gifts);
+
+*/
+
+function countDown(num){
+
+    while(num >= 0){
+        console.log(num);
+        num--;
+    }
+}
+
+function writeCards(names, giftType){
+
+    const newArray = [];
+    for(let i= 0; i < names.length; i++){
+
+        
+        newArray[i] = `Thank you, ${names[i]}, for the wonderful ${giftType} gift!`
+
+        
+    }
+    return newArray;
+}
